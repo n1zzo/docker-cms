f828564470662998c0ec441ae699975153b2aaac
William Di Luigi <williamdiluigi@gmail.com>
2019-11-10 11:50:46 +0100

    Still not working lol

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM alpine:latest
 MAINTAINER Niccolò Izzo <izzo.niccolo@gmail.com>
 
 RUN apk add --no-cache python3 git
+RUN python3 -m ensurepip
 RUN git clone --recursive https://github.com/cms-dev/cms /cms || true
 EXPOSE 8888 8889 8890
 ADD entrypoint.sh ./
-ENTRYPOINT ["entrypoint.sh"]
+ENTRYPOINT ["./entrypoint.sh"]
+CMD ["cms"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,13 @@
 
 # Script to check requirements and install cms from sources
 
-pip install -r requirements.txt
-python3 setup.py build
-python3 setup.py install
+set -e
+
+if [ "$1" = 'cms' ]; then
+    pip install -r requirements.txt
+    python3 setup.py build
+    python3 setup.py install
+    cmsResourceService -a
+fi
+
+exec "$@"
